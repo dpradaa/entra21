7f8328493a7e7bec55417c14072d9921088bbe47
dpradaa <144067042+dpradaa@users.noreply.github.com>
2023-09-11 18:27:07 -0300

    Add files via upload

---
+++ exercicioexemplo.java
@@ -0,0 +1,18 @@
+package exemplo;
+
+import java.util.Scanner;
+
+public class exercicioexemplo {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("inicio");
+		String opcao = scanner.next(); 
+		while (opcao.equals)  {
+			System.out.println("oi");
+			cont++;
+		}
+	}
+
+}
